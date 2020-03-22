5e935a7fe9ddede9a19c50aa6e90011ff3162a15
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-03-22 21:47:39 +0100

    Added obstacle generator

---
+++ Assets/Obstacles/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    [SerializeField] Obstacle obstacle;
+    public int numberOfObjects;
+    public int distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var pos = transform.position;
+        var rot = transform.rotation;
+        for (int i = 1; i <= numberOfObjects; i++)
+        {
+            pos.x += distance;
+            Instantiate(obstacle, pos, rot);
+        }
+    }
+
+}
