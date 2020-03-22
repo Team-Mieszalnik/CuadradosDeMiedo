5e31129daa88b188cac30ac54676a28c352f3fb3
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-03-22 22:21:50 +0100

    Added camera rotation togle

---
+++ Assets/OtherScripts/cameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public bool rotationOn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!rotationOn) transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
