64e08cfc9d196dd0b014d88cb861f86cb96065b0
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-03-22 22:07:48 +0100

    Added new hero prefab + tidying up

--- Assets/Obstacles/Scripts/ObstacleGenerator.cs
+++ Assets/Obstacles/Scripts/ObstacleGenerator.cs
@@ -7,10 +7,12 @@ public class ObstacleGenerator : MonoBehaviour
     [SerializeField] Obstacle obstacle;
     public int numberOfObjects;
     public int distance;
+    public int hpOfObjects;
 
     // Start is called before the first frame update
     void Start()
     {
+        obstacle.hp = hpOfObjects;
         var pos = transform.position;
         var rot = transform.rotation;
         for (int i = 1; i <= numberOfObjects; i++)
