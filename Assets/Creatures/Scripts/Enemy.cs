c491ea56b3e12ec8a78b89f6bf4a53aa9f9ec8bb
michszo529 <32485285+michszo529@users.noreply.github.com>
2020-03-28 16:35:42 +0100

    Monsters animations
    Added idle circle animation, damage animation in progress

--- Assets/Creatures/Scripts/Enemy.cs
+++ Assets/Creatures/Scripts/Enemy.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class Enemy : Creature
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+
+    protected override IEnumerator GetDamageAnimation()
+    {
+        if (health <= 0)
+        {
+            animator.SetBool("dead", true);
+
+            //postac umarla i nie zyje
+            //i co dalej?
+            AfterDeath();
+
+        }
+        else
+        {
+            animator.SetBool("getDamage", true);
+
+            yield return new WaitForSeconds(0.5F);
+
+            animator.SetBool("getDamage", false);
+        }
+    }
+
 }
