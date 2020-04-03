89f4c752039a97951b68abe52ac59cdb9c62dfdb
Swagsen <crazy456.ml@gmail.com>
2020-04-03 10:57:35 +0200

    MainMenu Settings
    + changing volume

--- Assets/MainMenu/OptionsMenu.cs
+++ Assets/MainMenu/OptionsMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
     Resolution[] resolutions;
     public Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
 
     private void Start()
     {
@@ -31,15 +34,14 @@ public class OptionsMenu : MonoBehaviour
         resolutionDropdown.RefreshShownValue();
     }
 
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
 
     public void SetMusic(float volume)
     {
-
-    }
-
-    public void SetVolume(float volume)
-    {
-
+        audioMixer.SetFloat("Music", volume);
     }
 
     public void SetQuality(float quality)
