f4e0ccc02be3d31cdd75d751b10587019287e432
michszo529 <32485285+michszo529@users.noreply.github.com>
2020-04-04 19:42:55 +0200

    alternativeAttack2
    Added triangle alternative attack

--- Assets/Creatures/Scripts/Triangle.cs
+++ Assets/Creatures/Scripts/Triangle.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Triangle : Enemy
 {
+    protected override IEnumerator AlternativeAttack()
+    {
+        attack = true;
+        animator.SetBool("attack", true);
+        yield return new WaitForSeconds(1.5F);//animation time
 
+        for(int i=0; i<8; i++)
+        {
+            float angle = i * Mathf.PI * 14;
+            Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, angle)); // obiekt, pozycja startowa, kierunek
+		    
+        }
+
+        animator.SetBool("attack", false);
+        yield return new WaitForSeconds(Random.Range(2F, 4F));//attack time
+        attack = false;
+    }
 }
