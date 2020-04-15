e10060ec357e4e854ceab2aa023c0932ac66cc54
Swagsen <crazy456.ml@gmail.com>
2020-04-15 11:48:57 +0200

    Boss HP bar

---
+++ Assets/Creatures/Scripts/BossHPBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHPBar : MonoBehaviour
+{
+    public Creature creature;
+    private Slider hp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = GetComponent<Slider>();
+        hp.maxValue = creature.healthMax;
+        hp.value = creature.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp.value = creature.health;
+    }
+}
