cb97ebd8e43e89b5a29f7754f32924a642c2f689
Swagsen <crazy456.ml@gmail.com>
2020-04-15 19:25:40 +0200

    Loading Scene update

--- Assets/LoadingMenu/LoadingMenu.cs
+++ Assets/LoadingMenu/LoadingMenu.cs
@@ -8,6 +8,10 @@ public class LoadingMenu : MonoBehaviour
 {
 
     public TextMeshProUGUI mapName;
+    public TextMeshProUGUI levelName;
+    public Image map;
+
+    public Sprite[] mapPrevies;
 
     public void ContinueGame()
     {
@@ -17,5 +21,27 @@ public class LoadingMenu : MonoBehaviour
     public void Update()
     {
         mapName.text = MapController.SceneName;
+        levelName.text = "LEVEL " + LevelController.Level.ToString();
+        map.sprite = StringToSprite(MapController.SceneName);
     }
+
+    private Sprite StringToSprite(string mapName)
+    {
+
+        switch (mapName)
+        {
+            case "Arena":
+                return mapPrevies[0];
+            case "Invierno":
+                return mapPrevies[1];
+            case "Primavera":
+                return mapPrevies[2];
+            case "Vesuvio":
+                return mapPrevies[3];
+
+        }
+        return mapPrevies[0];
+    }
+
+
 }
