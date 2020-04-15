bf85e43b551c8a33dfe4a36b12880145263b1d0c
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-04-15 14:25:23 +0200

    added new soundeffects
    flamethrower, shield, heal, death
    known bug - flamethrower loop not working properly

--- Assets/Guns/Scripts/CastWeapons.cs
+++ Assets/Guns/Scripts/CastWeapons.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class CastWeapons : Weapon
 {
     public GameObject cast;
+    private bool isLoop = false;
+    public AudioClip startClip;
+    public AudioClip loopClip;
+
+    private AudioSource ASstart;
+    private AudioSource ASloop;
+
 
     private void Awake()
     {
         cast.SetActive(false);
+        ASstart = gameObject.AddComponent<AudioSource>();
+        ASstart.clip = startClip;
+        ASstart.loop = false;
+        ASloop = gameObject.AddComponent<AudioSource>();
+        ASloop.clip = loopClip;
+        ASloop.loop = true;
     }
 
     protected override void Control()
@@ -16,16 +29,32 @@ public class CastWeapons : Weapon
         if (Input.GetMouseButton(0))
         {
             cast.SetActive(true);
-            if (!audioSource.isPlaying)
-                audioSource.Play();
+            Loop();
             // Shoot();
             //StartCoroutine(ShootAnimation());
             ammo -= 10 * Time.deltaTime;
         }
         else
         {
-            audioSource.Stop();
+            StopLoop();
             cast.SetActive(false);
         }
+    }
+
+    float initialDelay = 0.1f;
+    private void Loop()
+    {
+        if (isLoop == false)
+        {
+            isLoop = true;
+            ASstart.Play();
+            ASloop.PlayDelayed(startClip.length-initialDelay);
+        }
+    }
+    private void StopLoop()
+    {
+        isLoop = false;
+        ASstart.Stop();
+        ASloop.Stop();
     }
 }
