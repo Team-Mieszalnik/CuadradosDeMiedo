262462adae44723f59a628a68fa3e442f092ea2d
michszo529 <32485285+michszo529@users.noreply.github.com>
2020-04-15 13:41:23 +0200

    Spawns
    Bosses can now spawn enemies, enemies can't be spawned via loot script

--- Assets/Creatures/Scripts/TriangleBoss.cs
+++ Assets/Creatures/Scripts/TriangleBoss.cs
@@ -8,11 +8,19 @@ public class TriangleBoss : Triangle
 {
     protected override IEnumerator Attack()
     {
-        attacking = AlternativeAttack;
+        switch (Random.Range(0, 100))
+        {
+            case int n when (n > 10):
+                attacking = StandardAttack;
+                break;
+            default:
+                attacking = AlternativeAttack;
+                break;
+        }
         yield return new WaitForSeconds(0.1F);
     }
 
-    protected override IEnumerator AlternativeAttack()
+    protected override IEnumerator StandardAttack()
     {
         light2D = GetComponent<Light2D>();
         attack = true;
@@ -26,6 +34,25 @@ public class TriangleBoss : Triangle
             Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, angle)); // obiekt, pozycja startowa, kierunek
 		    
         }
+
+        animator.SetBool("attack", false);
+        light2D.intensity = 0;
+        yield return new WaitForSeconds(Random.Range(1F, 2F));//attack time
+        attack = false;
+    }
+    protected override IEnumerator AlternativeAttack()
+    {
+        light2D = GetComponent<Light2D>();
+        attack = true;
+        animator.SetBool("attack", true);
+        light2D.intensity = 1;
+        yield return new WaitForSeconds(1.5F);//animation time
+
+        Vector3 newPosition = new Vector3();
+        newPosition = transform.position;
+        newPosition.x += 3;
+        newPosition.y += 3;
+        Instantiate(spawnable, newPosition, Quaternion.identity);
 
         animator.SetBool("attack", false);
         light2D.intensity = 0;
