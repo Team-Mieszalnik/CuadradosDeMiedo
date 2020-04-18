76b86d08f3b6a34e356d8aa95aa64399901ba08a
Swagsen <crazy456.ml@gmail.com>
2020-04-18 12:43:24 +0200

    Minimap
    added minimap and map previe in game

---
+++ Assets/HUD/Scripts/MiniMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Hero").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //transform.position = player.position;
+
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
