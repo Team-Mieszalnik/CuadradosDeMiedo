8deb9d06f95067709967e7cf335292ef8b4ceefd
Swagsen <crazy456.ml@gmail.com>
2020-04-27 13:27:44 +0200

    hud tooltip update
    slider for current level and tooltip for every skill

--- Assets/HUD/Scripts/HUD.cs
+++ Assets/HUD/Scripts/HUD.cs
@@ -101,7 +101,7 @@ public class HUD : MonoBehaviour
             miniMap.SetActive(isMiniMapVisible);
         }
 
-        //if (Input.GetKeyDown(KeyCode.O)) Experience.points++;
+        if (Input.GetKeyDown(KeyCode.O)) Experience.points++;
 
     }
 
---
+++ Assets/HUD/Scripts/SkillLevelSlider.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillLevelSlider : MonoBehaviour
+{
+
+    public Slider skillSlider;
+    private Experience experience;
+
+    public enum SkillType {HEALTH, ENERGY, ARMOR, MOVEMENT, CURE};
+
+    public SkillType skillType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        experience = GameObject.FindGameObjectWithTag("Hero").GetComponent<Experience>();
+
+        switch(skillType)
+        {
+            case SkillType.HEALTH:
+                skillSlider.maxValue = experience.healthMaxLevel;
+                skillSlider.value = experience.healthLevel;
+                break;
+            case SkillType.ENERGY:
+                skillSlider.maxValue = experience.energyhMaxLevel;
+                skillSlider.value = experience.energyLevel;
+                break;
+            case SkillType.MOVEMENT:
+                skillSlider.maxValue = experience.movementMaxLevel;
+                skillSlider.value = experience.movementLevel;
+                break;
+            case SkillType.ARMOR:
+                skillSlider.maxValue = experience.armorMaxLevel;
+                skillSlider.value = experience.armorLevel;
+                break;
+            case SkillType.CURE:
+                skillSlider.maxValue = experience.cureMaxLevel;
+                skillSlider.value = experience.cureLevel;
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (skillType)
+        {
+            case SkillType.HEALTH:
+                skillSlider.maxValue = experience.healthMaxLevel;
+                skillSlider.value = experience.healthLevel;
+                break;
+            case SkillType.ENERGY:
+                skillSlider.maxValue = experience.energyhMaxLevel;
+                skillSlider.value = experience.energyLevel;
+                break;
+            case SkillType.MOVEMENT:
+                skillSlider.maxValue = experience.movementMaxLevel;
+                skillSlider.value = experience.movementLevel;
+                break;
+            case SkillType.ARMOR:
+                skillSlider.maxValue = experience.armorMaxLevel;
+                skillSlider.value = experience.armorLevel;
+                break;
+            case SkillType.CURE:
+                skillSlider.maxValue = experience.cureMaxLevel;
+                skillSlider.value = experience.cureLevel;
+                break;
+        }
+    }
+}
