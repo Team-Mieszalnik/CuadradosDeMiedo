424aa311b647e8494716f3bca1c626dedd4f1a9b
Swagsen <crazy456.ml@gmail.com>
2020-05-10 19:39:03 +0200

    added stats menu

---
+++ Assets/MainMenu/Scripts/StatisticMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatisticMenu : MonoBehaviour
+{
+
+    public TextMeshProUGUI highestLevel;
+    public TextMeshProUGUI bestTime;
+    public TextMeshProUGUI enemiesKilled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highestLevel.text = StatsManager.HighestLevel.ToString();
+        bestTime.text = StatsManager.BestTime.ToString();
+        enemiesKilled.text = StatsManager.EnemiesKilled.ToString();
+    }
+
+}
---
+++ Assets/MainMenu/Scripts/StatsManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager
+{
+
+    public static float BestTime
+    {
+        get => PlayerPrefs.GetFloat("BestTime", 0);
+        set
+        {
+            float prevBestTime = PlayerPrefs.GetFloat("BestTime", 0);
+
+            if (value < prevBestTime)
+                PlayerPrefs.SetFloat("BestTime", value);
+        }
+    }
+
+    public static int HighestLevel
+    {
+        get => PlayerPrefs.GetInt("HighestLevel", 0);
+        set
+        {
+            int prevBestLevel = PlayerPrefs.GetInt("HighestLevel", 0);
+
+            if (value > prevBestLevel)
+                PlayerPrefs.SetInt("HighestLevel", value);
+        }
+    }
+
+    public static int EnemiesKilled
+    {
+        get => PlayerPrefs.GetInt("EnemiesKilled", 0);
+        set
+        {
+            int prevKills = PlayerPrefs.GetInt("EnemiesKilled", 0);
+            prevKills += value;
+            PlayerPrefs.SetInt("EnemiesKilled", prevKills);
+        }
+    }
+
+}
