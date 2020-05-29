ee1e573b67fcba8e7a85036d9b53206c682a499a
Dominik <dominikbis1998@o2.pl>
2020-05-29 17:29:32 +0200

    Cueva fix
    - fixed map fix
    - added AbyssSurfacess

---
+++ Assets/Map/SurfacesScripts/AbyssSurfaces.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbyssSurfaces : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "EnemyBullet" || collision.gameObject.tag == "Triangle")
+        {
+            return;
+        }
+
+        IGetDamaged target = collision.GetComponent<IGetDamaged>();
+
+        if (target != null)
+        {
+            target.GetDamage(10000);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "EnemyBullet" || collision.gameObject.tag == "Triangle")
+        {
+            return;
+        }
+
+
+        IGetDamaged target = collision.GetComponent<IGetDamaged>();
+
+        if (target != null)
+        {
+            target.GetDamage(10000);
+        }
+    }
+}
